58706528b4999c7250349be4835de90535bd06ee
Josh Bickett <josh@bickett.net>
2023-01-21 11:44:06 -0800

    Simplify enter until fade text works and move to emotion.styled

--- src/Entrance.js
+++ src/Entrance.js
@@ -1,8 +1,28 @@
 import { CSSTransition } from "react-transition-group";
 import { getMansionImage } from "./utils/rss/gamesManager";
 import { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+
+const texts = ["Text 1", "Text 2", "Text 3"];
 
 export const Entrance = ({ setEntered }) => {
+  const [text, setText] = useState("");
+  const [top, setTop] = useState("0px");
+  const [left, setLeft] = useState("0px");
+  const [visible, setVisible] = useState(false);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     const randomIndex = Math.floor(Math.random() * texts.length);
+  //     const newText = texts[randomIndex];
+  //     setText(newText);
+  //     setTop(`${Math.floor(Math.random() * (window.innerHeight - 100))}px`);
+  //     setLeft(`${Math.floor(Math.random() * (window.innerWidth - 100))}px`);
+  //     setVisible(!visible);
+  //   }, 2000);
+  //   return () => clearInterval(interval);
+  // }, [visible]);
+
   const img = getMansionImage();
   const [isFlipped, setIsFlipped] = useState(false);
 
@@ -19,22 +39,8 @@ export const Entrance = ({ setEntered }) => {
   }, [isFlipped, setEntered]);
 
   return (
-    <div className="enter-container">
-      <div className="subheader">
-        {!isFlipped && (
-          <div>
-            <br />
-            <br />
-            <p>Gather family or friends for some fun!</p>
-            <br />
-            <p>Leave the screen behind and play live</p>
-            <br />
-            <br />
-            <br />
-            <p>Try something different</p>
-          </div>
-        )}
-      </div>
+    <Container>
+      <h1>Enter</h1>
 
       <CSSTransition
         in={!isFlipped}
@@ -45,19 +51,42 @@ export const Entrance = ({ setEntered }) => {
         <img
           src={img}
           alt="enter"
-          style={{ width: "400px", borderRadius: "6px", flex: "1" }}
+          style={{ width: "200px", borderRadius: "6px" }}
+          onMouseOver={handleFlip}
         />
       </CSSTransition>
 
-      {!isFlipped && (
-        <div className="App-link" onClick={handleFlip}>
-          ENTER
-        </div>
-      )}
+      <div></div>
+    </Container>
+  );
+};
 
-      <div className="subheader ">
-        {!isFlipped && (
-          <div>
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  background-color: #131619;
+  color: white;
+  text-align: center;
+  overflow: hidden;
+`;
+
+/*  <div>
+            <br />
+            <br />
+            <p>Gather family or friends for some fun!</p>
+            <br />
+            <p>Leave the screen behind and play live</p>
+            <br />
+            <br />
+            <br />
+            <p>Try something different</p>
+          </div> */
+
+/*  <div>
             <p>Play a murder mystery game</p>
             <br />
             <br />
@@ -71,9 +100,4 @@ export const Entrance = ({ setEntered }) => {
             <br />
             <br />
             <p>Enter now to browse</p>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+          </div> */
