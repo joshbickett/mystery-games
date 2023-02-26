724d308b23f0bda709d31d7786da6f5893cc44bf
Josh Bickett <josh@bickett.net>
2023-02-26 10:26:46 -0800

    Add additional elements to CharacterDetail component

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,3 +1,29 @@
+import styled from "@emotion/styled";
+
 export const CharacterDetail = ({ character }) => {
-  return <div>{character.name}</div>;
+  return (
+    <Container>
+      <h1>{character.name}</h1>
+      <CharacterImg src={character.img} alt={character.name} />
+      <h4>{character.description}</h4>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: calc(10px + 2vmin);
+  background-color: #ffffff;
+  color: black;
+  text-align: center;
+  overflow: hidden;
+`;
+
+const CharacterImg = styled.img`
+  height: 300px;
+  border-radius: 16px;
+`;
