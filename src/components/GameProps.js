803c3d5a16aca7039faa115c6259e931180aa4b6
Josh Bickett <josh@bickett.net>
2023-02-28 22:10:36 -0800

    Add initial container for contents of gameProps

--- src/components/GameProps.js
+++ src/components/GameProps.js
@@ -1,7 +1,19 @@
+import styled from "@emotion/styled";
+
 export const GameProps = (props) => {
   return (
-    <div>
+    <Container>
       <h1>2. Game Props</h1>
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  color: white;
+  background-color: black;
+  align-items: center;
+  justify-content: center;
+`;
