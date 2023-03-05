887ccab0e87c3def805610125935e9e8d7736602
Josh Bickett <josh@bickett.net>
2023-03-05 12:13:40 -0800

    Refine instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -22,15 +22,15 @@ export const Instructions = () => {
             color: "black",
             padding: "20px 40px",
             backgroundColor: "rgb(249, 249, 249)",
+            alignItems: "flex-start",
           }}
         >
-          <h1 style={{ margin: 0, padding: 0 }}>Your job</h1>
+          <h1 style={{ margin: 0, padding: 0, textAlign: "left" }}>Your job</h1>
           <InstructionText>
-            <strong>1.</strong> Choose a physical location to host the Mystery
-            Game at
+            <strong>1.</strong> Choose a location for the game
           </InstructionText>
           <InstructionText>
-            <strong>2.</strong> Decide how many players are joining
+            <strong>2.</strong> Decide how many players will be playing
           </InstructionText>
           <InstructionSubText>
             The game is setup to allow for between 7-9 players. You will notice
@@ -48,8 +48,7 @@ export const Instructions = () => {
             the instructions for the players about how to play and win.
           </InstructionSubText>
           <InstructionText>
-            <strong>4.</strong> Print off the props and place them around the
-            location of the game.
+            <strong>4.</strong> Print off the props and place them around
           </InstructionText>
           <InstructionSubText>
             The game props can be found at the bottom of this page. Once you
@@ -58,8 +57,7 @@ export const Instructions = () => {
             of print offs, that is a welcomed and fun idea as well.
           </InstructionSubText>
           <InstructionText>
-            <strong>5.</strong> Before the game starts check with the group if
-            anyone has questions about how to play and win.
+            <strong>5.</strong> Check with the group if there are questions
           </InstructionText>
           <InstructionSubText>
             In the character cards you sent each player there is instructions on
@@ -76,18 +74,16 @@ export const Instructions = () => {
             adjust it to be shorter or longer depending on the group.
           </InstructionSubText>
           <InstructionText>
-            <strong>7.</strong> As the fortune teller, may answer one yes / or
-            question throughout the game to help the players.
+            <strong>7.</strong> You may answer one yes or no question
           </InstructionText>
           <InstructionSubText>
-            Each player may ask you *one* yes or no question. If you prefer you
-            can leave this part of the game out, but it adds a fun twist and
-            allows you to help if the players are struggling to find the
-            villian, motive, and method.
+            As the fortune teller, each player may ask you *one* yes or no
+            question. If you prefer you can leave this part of the game out, but
+            it adds a fun twist and allows you to help if the players are
+            struggling to find the villian, motive, and method.
           </InstructionSubText>
           <InstructionText>
-            <strong>8.</strong> Most importantly, get into character and enjoy
-            yourself
+            <strong>8.</strong> Get into character and enjoy yourself
           </InstructionText>
           <InstructionSubText>
             Though you are the game host, you are also a character in the game
@@ -109,10 +105,12 @@ export const Instructions = () => {
 
 const InstructionText = styled.p`
   font-size: 18px;
+  text-align: left;
 `;
 const InstructionSubText = styled.p`
   font-size: 14px;
   margin: 0;
+  text-align: left;
   padding: 0;
   margin-left: 20px;
 `;
